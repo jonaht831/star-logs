034d717896f23a2e4ac99c947e25344268fb4b36
Jake Overall <joverall22@gmail.com>
2018-08-30 10:22:13 -0600

    adds corsOptions

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var server = express()
 var cors = require('cors')
 var port = 3000
 
-server.use(cors())
+var whitelist = ['http://localhost:8080'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+server.use(cors(corsOptions))
 
 //Fire up database connection
 require('./server-assets/db/mlab-config')
